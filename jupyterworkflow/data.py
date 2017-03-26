09774be2156917187c4621c0ce3a7a99f0acb10a
Sheeroh <shirojean@gmail.com>
2017-03-26 15:10:58 -0500

    refactor data download

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -24,12 +24,17 @@ def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=Fal
    data: pandas pandas.DataFrame 
       The fremont bridge data
    """
-
-     if  force_download or not os.path.exists(filename):
-           urlretrieve(url,filename)
-     data = pd.read_csv ('Fremont.csv', index_col='Date', parse_dates=True)
+     if force_download or not os.path.exists(filename):
+         urlretrieve(url,filename)
+     data = pd.read_csv ('Fremont.csv', index_col='Date')
+     try:
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %H:%M:%S %p')
+     except TypeError:
+        data.index = pd.to_datetime[data.index]
      data.columns =['West', 'East']
      data ['Total'] =data['West']+data['East']
      return data
+
+
 
 
